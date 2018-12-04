c9ebc937eea3bcdab261496e4cdd60129f17024a
zhuoxiaolei <1078528010@qq.com>
2018-12-04 17:00:28 +0800

    dd

---
+++ testFile/test.h
@@ -0,0 +1,17 @@
+//
+//  test.h
+//  creatPodTest
+//
+//  Created by HaoSheng on 2018/12/4.
+//  Copyright © 2018 HaoSheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface test : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ testFile/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  creatPodTest
+//
+//  Created by HaoSheng on 2018/12/4.
+//  Copyright © 2018 HaoSheng. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
